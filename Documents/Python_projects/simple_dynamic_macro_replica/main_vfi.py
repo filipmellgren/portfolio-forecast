fb1b2718e7bbf57dc86538463cba20b01473d34b
filipmellgren <filip.mellgren@gmail.com>
2019-11-26 21:40:10 +0100

    Test commit

---
+++ Documents/Python_projects/simple_dynamic_macro_replica/main_vfi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 21:39:26 2019
+
+@author: filip
+!git add "file.py"
+!git commit -m "My commit"
+!git push origin master
+"""
+
